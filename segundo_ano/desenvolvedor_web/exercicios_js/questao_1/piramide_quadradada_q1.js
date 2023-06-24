91c8b419eab04bc8d680a0263f95707c12be8fd7
kauaa <117315707+kauuaa@users.noreply.github.com>
2023-06-24 00:45:38 -0300

    Delete segundo_ano/desenvolvedor_web/exercicios_js/questao_1 directory

--- segundo_ano/desenvolvedor_web/exercicios_js/questao_1/piramide_quadradada_q1.js
+++
@@ -1,7 +0,0 @@
-function calcular(){
-    let lado = Number(document.getElementById('lado').value)
-    let altura = Number(document.getElementById('altura').value)
-
-    let volume = (lado * altura) / 3
-    document.getElementById("res").innerHTML = "O volume da pirâmide é = " + volume
-}
