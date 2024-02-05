026f1af3ae296fe43de24cb91f52fb8195009674
Kauã Biasebetti <kauamengel17@gmail.com>
2024-02-05 10:18:21 -0300

    arraysBackend

---
+++ terceiro_ano/desenvolvimento_de_aplicativos/05-02-24/array.js
@@ -0,0 +1,16 @@
+let cidade = []
+
+cidade[0] = 'Soledade'
+cidade[1] = 'Xique-Xique'
+cidade[2] = 'Itaquaquecetuba'
+
+console.log(cidade.length)
+console.log('Cidade = ' + cidade.join(', '))
+console.log(cidade.indexOf('Soledade'))
+
+let roupas = new Array()
+let roupas2 = new Array('Sapato', 'camiseta', 'tênis',)
+console.log(`Array 1 = ${roupas}`)
+console.log(`Array 2 = ${roupas2.join(', ')}`)
+
+let primeiroTestePrograma = 0
