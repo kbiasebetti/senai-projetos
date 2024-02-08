d37c22f39e3af93f5071f7554bd0a5bf27e7343c
Kauã Biasebetti <kauamengel17@gmail.com>
2024-02-08 15:17:03 -0300

    Add files via upload

---
+++ terceiro-ano/desenvolvimento-de-aplicativos/07-02-24/clienteClass.js
@@ -0,0 +1,19 @@
+class Pessoa {
+    constructor(nome, idade, altura) {
+        this.nome = nome
+        this.idade = idade
+        this.altura = altura
+    }
+    
+    mensagem() {
+        console.log("Olá")
+    }
+
+    teste(){
+        let valor = 2
+        let num = 3
+        let resposta = valor + num
+
+        return resposta
+    }
+}
